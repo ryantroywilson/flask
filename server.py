306a2038a1406a8497e2661067277d9b5e87c608
ryantroywilson <ryantroywilson@gmail.com>
2017-07-16 11:08:50 -0700

    Add files via upload

--- server.py
+++ server.py
@@ -1,7 +1,25 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, redirect, request, session
 
-@app.route ('/')
+app = Flask(__name__)
+app.secret_key = 'key'
+
+@app.route('/')
 def index():
-	return render_template("index.html", phrase='Hello', times=4, name = 'Ryan')
+   if 'counter' not in session:
+       session['counter'] = 0
+   session['counter'] += 1
+   return render_template('index.html')
+
+@app.route('/ninja')
+def ninja():
+ 	if 'counter' not in session:
+  		session['counter'] = 0
+  	session9['counter'] += 2
+  	return render_template('ninja.html')
+
+@app.route('/hacker', methods=['POST'])
+def reset():
+   session.clear()
+   return redirect('/hacker')
+
 app.run(debug=True)
